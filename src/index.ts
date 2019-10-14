c0aa3b8c292b4c981f7e3a4b8aef72c591bcb5ec
shybyte <ein@volloeko.de>
2019-10-14 05:55:45 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+export type Selector<T, K> = (x: T) => K;
+
+export class SearchResult {
+  constructor(public readonly found: boolean, public readonly index: number) {}
+}
+
+export function binarySearchBy<T, K extends number | string>(
+  haystack: ArrayLike<T>,
+  needle: K,
+  selector: Selector<T, K>,
+  low = 0,
+  high = haystack.length - 1
+): SearchResult {
+  if (haystack.length === 0) {
+    return { found: false, index: 0 };
+  }
+
+  if (low < 0 || low >= haystack.length) {
+    throw new RangeError('Invalid lower bound.');
+  }
+
+  if (high < low || high >= haystack.length) {
+    throw new RangeError('Invalid upper bound.');
+  }
+
+  while (low <= high) {
+    const mid = low + ((high - low) >>> 1);
+    const midK = selector(haystack[mid]);
+
+    if (midK < needle) {
+      low = mid + 1; // Too low
+    } else if (midK > needle) {
+      high = mid - 1; // Too high
+    } else {
+      return new SearchResult(true, mid); // Found
+    }
+  }
+
+  return new SearchResult(false, low); // Not found
+}
+
+export const identity = <T>(x: T) => x;
---
+++ test/index.test.ts
@@ -0,0 +1,61 @@
+import { binarySearchBy, identity } from '../src';
+
+test('empty', () => {
+  expect(binarySearchBy([], 1, identity)).toEqual({ found: false, index: 0 });
+});
+
+describe('one element', () => {
+  test('equal', () => {
+    expect(binarySearchBy([1], 1, identity)).toEqual({ found: true, index: 0 });
+  });
+
+  test('larger', () => {
+    expect(binarySearchBy([2], 1, identity)).toEqual({
+      found: false,
+      index: 0
+    });
+  });
+
+  test('smaller', () => {
+    expect(binarySearchBy([0], 1, identity)).toEqual({
+      found: false,
+      index: 1
+    });
+  });
+});
+
+describe('5 elements', () => {
+  const haystack = [0, 2, 4, 6, 8];
+
+  test('find existing values', () => {
+    haystack.forEach((value, i) => {
+      expect(binarySearchBy(haystack, value, identity)).toEqual({
+        found: true,
+        index: i
+      });
+    });
+  });
+
+  test('find insertion point of not existing values', () => {
+    haystack.forEach((value, i) => {
+      expect(binarySearchBy(haystack, value + 1, identity)).toEqual({
+        found: false,
+        index: i + 1
+      });
+    });
+  });
+
+  describe('error handling', () => {
+    test('throw range error if lower bound is to low', () => {
+      expect(() => {
+        binarySearchBy(haystack, 0, identity, -1);
+      }).toThrow();
+    });
+
+    test('throw range error if upper bound bound is to high', () => {
+      expect(() => {
+        binarySearchBy(haystack, 0, identity, 0, haystack.length);
+      }).toThrow();
+    });
+  });
+});
